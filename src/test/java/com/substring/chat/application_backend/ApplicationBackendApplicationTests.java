43cac331ae7c3a6179eac179c703e9ccdcedd5e8
chitizawasthi <chitizawasthi@gmail.com>
2025-10-25 22:38:18 +0530

    Initial commit

---
+++ src/test/java/com/substring/chat/application_backend/ApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.substring.chat.application_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
